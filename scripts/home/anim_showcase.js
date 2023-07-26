9594edfb41717771a043a95ff6159ade2109dd09
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-07-26 21:29:30 +0800

    homepage design

--- scripts/home/anim_showcase.js
+++ scripts/home/anim_showcase.js
@@ -1,27 +1,31 @@
 var cur_obj;
+var _pos;
 
 function movein_showcasetitle(cur_showcase) {
     switch (cur_showcase) {
-        case 1 : cur_obj = document.getElementById("HomepageCenterShowcaseTitle"); break;
-        case 2 : cur_obj = document.getElementById("HomepageLeftShowcaseTitle"); break;
-        case 3 : cur_obj = document.getElementById("HomepageRightShowcaseTitle"); break;
+        case 1 : cur_obj = document.getElementById("homepageCenterShowcaseTitle"); break;
+        case 2 : cur_obj = document.getElementById("homepageLeftShowcaseTitle"); break;
+        case 3 : cur_obj = document.getElementById("homepageRightShowcaseTitle"); break;
     }
+    console.log(cur_obj.id);
     requestAnimationFrame(anim_movein_showcasetitle);
 }
 
 function moveout_showcasetitle(cur_showcase) {
     switch (cur_showcase) {
-        case 1 : cur_obj = document.getElementById("HomepageCenterShowcaseTitle"); break;
-        case 2 : cur_obj = document.getElementById("HomepageLeftShowcaseTitle"); break;
-        case 3 : cur_obj = document.getElementById("HomepageRightShowcaseTitle"); break;
+        case 1 : cur_obj = document.getElementById("homepageCenterShowcaseTitle"); break;
+        case 2 : cur_obj = document.getElementById("homepageLeftShowcaseTitle"); break;
+        case 3 : cur_obj = document.getElementById("homepageRightShowcaseTitle"); break;
     }
+    console.log(cur_obj.id);
     requestAnimationFrame(anim_moveout_showcasetitle);
 }
 
 function anim_movein_showcasetitle(_t) {
     let delta_t = performance.now() - _t;
+    _pos += delta_t;
     if (cur_obj.style.bottom < 0) {
-        cur_obj.style.bottom += 0.3 * delta_t + 'px';
+        cur_obj.style.bottom = _pos + 'px';
         requestAnimationFrame(anim_movein_showcasetitle);
         return;
     } else cur_obj.style.bottom = 0;
@@ -30,8 +34,9 @@ function anim_movein_showcasetitle(_t) {
 
 function anim_moveout_showcasetitle(_t) {
     let delta_t = performance.now() - _t;
+    _pos -= delta_t;
     if (cur_obj.style.top > 0) {
-        cur_obj.style.top -= 0.3 * delta_t + 'px';
+        cur_obj.style.top = _pos + 'px';
         requestAnimationFrame(anim_moveout_showcasetitle);
         return;
     } else cur_obj.style.top = 0;
