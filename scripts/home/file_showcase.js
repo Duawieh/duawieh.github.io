c37a573c0480a2e34126df13bede87f7032012da
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-07-30 19:01:43 +0800

    homepage design

--- scripts/home/file_showcase.js
+++ scripts/home/file_showcase.js
@@ -1,1 +1,7 @@
-$("#homepageCenterShowcaseTitle_Title").load("./test.txt");
+$("#homepageCenterShowcaseTitle_Title").load("./contents/homepageShowcase/showcase1/title.txt");
+$("#homepageLeftShowcaseTitle_Title").load("./contents/homepageShowcase/showcase2/title.txt");
+$("#homepageRightShowcaseTitle_Title").load("./contents/homepageShowcase/showcase3/title.txt");
+
+$("#homepageCenterShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase1/subtitle.txt");
+$("#homepageLeftShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase2/subtitle.txt");
+$("#homepageRightShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase3/subtitle.txt");
