abfeb232992f19087f62553e75a6bcc9473f00af
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-07-30 18:31:46 +0800

    aboutpage design

--- scripts/home/file_showcase.js
+++ scripts/home/file_showcase.js
@@ -1,11 +1,26 @@
-function loadFile() {
-    // fetch('../../contents/home/showcase1/title.txt')
-    fetch('./contents/home/showcase1/title.txt')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('homepageCenterShowcaseTitle_Title').textContent = data;
-      })
-      .catch(error => {
-        console.log('读取文件出错：', error);
-      });
-}
+$("#homepageCenterShowcaseTitle_Title").load("./test.txt");
+
+// function loadXMLDoc(){
+// 	var xmlhttp;
+// 	// 1，建立xmlHttpRequest对象
+// 	if(window.XMLHttpRequest){
+// 		xmlhttp = new XMLHttpRequest();
+// 	}
+// 	else{
+// 		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+// 	}
+// 	// 2，设置回调函数
+// 	xmlhttp.onreadystatechange=callback;
+// 	// 3，使用open方法与服务器建立连接
+// 	xmlhttp.open("GET","test.txt",true);
+// 	// 4，向服务器端发送数据
+// 	xmlhttp.send();
+// 	// 5，在回调函数中针对不同呃响应状态进行处理
+// 	function callback(){
+// 		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+// 		//注意： onreadystatechange 事件被触发 4 次（0 - 4）, 分别是： 0-1、1-2、2-3、3-4，对应着 readyState 的每个变化。
+// 			document.getElementById("homepageCenterShowcaseTitle_Title").innerHTML = xmlhttp.responseText;
+// 		}
+// 	}
+// }
+
--- scripts/topbuttons.js
+++ scripts/topbuttons.js
@@ -1,10 +1,3 @@
-var index_cur_topbutton = 1;
-
-function topButtonClick(index_topbutton) {
-    index_cur_topbutton = index_topbutton;
-    console.log(index_cur_topbutton);
-}
-
 function mouseOverButton(cur_button) {
     cur_button.style.color = '#55ffbb';
 }
