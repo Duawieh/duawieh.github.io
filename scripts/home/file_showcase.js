cc769300f450861e609e8dc4819327116e9e7cfb
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-07-30 21:23:13 +0800

    return back

--- scripts/home/file_showcase.js
+++ scripts/home/file_showcase.js
@@ -1,7 +1,7 @@
-$("#homepageCenterShowcaseTitle_Title").load("./contents/homepageShowcase/showcase1/title.txt #title");
-$("#homepageLeftShowcaseTitle_Title").load("./contents/homepageShowcase/showcase2/title.txt #title");
-$("#homepageRightShowcaseTitle_Title").load("./contents/homepageShowcase/showcase3/title.txt #title");
+$("#homepageCenterShowcaseTitle_Title").load("./contents/homepageShowcase/showcase1/title.txt");
+$("#homepageLeftShowcaseTitle_Title").load("./contents/homepageShowcase/showcase2/title.txt");
+$("#homepageRightShowcaseTitle_Title").load("./contents/homepageShowcase/showcase3/title.txt");
 
-$("#homepageCenterShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase1/title.txt #subtitle");
-$("#homepageLeftShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase2/title.txt #subtitle");
-$("#homepageRightShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase3/title.txt #subtitle");
+$("#homepageCenterShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase1/subtitle.txt");
+$("#homepageLeftShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase2/subtitle.txt");
+$("#homepageRightShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase3/subtitle.txt");
