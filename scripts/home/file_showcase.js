874603f4098dfac1357a5f57f018fcf4128b7796
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-07-31 10:35:48 +0800

    json test

--- scripts/home/file_showcase.js
+++ scripts/home/file_showcase.js
@@ -1,7 +1,27 @@
-$("#homepageCenterShowcaseTitle_Title").load("./contents/homepageShowcase/showcase1/title.txt");
-$("#homepageLeftShowcaseTitle_Title").load("./contents/homepageShowcase/showcase2/title.txt");
-$("#homepageRightShowcaseTitle_Title").load("./contents/homepageShowcase/showcase3/title.txt");
+// $("#homepageCenterShowcaseTitle_Title").load("./contents/homepageShowcase/showcase1/title.txt");
+// $("#homepageLeftShowcaseTitle_Title").load("./contents/homepageShowcase/showcase2/title.txt");
+// $("#homepageRightShowcaseTitle_Title").load("./contents/homepageShowcase/showcase3/title.txt");
 
-$("#homepageCenterShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase1/subtitle.txt");
-$("#homepageLeftShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase2/subtitle.txt");
-$("#homepageRightShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase3/subtitle.txt");
+// $("#homepageCenterShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase1/subtitle.txt");
+// $("#homepageLeftShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase2/subtitle.txt");
+// $("#homepageRightShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase3/subtitle.txt");
+
+$.getJSON("./contents/homepageShowcase/config.json", function(data) {
+    let showcase_title = new Array();
+    let showcase_subtitle = new Array();
+    let showcase_link = new Array();
+    showcase_title[0] = document.getElementById("homepageCenterShowcaseTitle_Title");
+    showcase_subtitle[0] = document.getElementById("homepageCenterShowcaseTitle_Subtitle");
+    showcase_link[0] = document.getElementById("homepageCenterShowcase_Link");
+    showcase_title[1] = document.getElementById("homepageLeftShowcaseTitle_Title");
+    showcase_subtitle[1] = document.getElementById("homepageLeftShowcaseTitle_Subtitle");
+    showcase_link[1] = document.getElementById("homepageLeftShowcase_Link");
+    showcase_title[2] = document.getElementById("homepageRightShowcaseTitle_Title");
+    showcase_subtitle[2] = document.getElementById("homepageRightShowcaseTitle_Subtitle");
+    showcase_link[2] = document.getElementById("homepageRightShowcase_Link");
+    $.each(data, function(index, info) {
+        showcase_title[index].innerHTML = info["title"];
+        showcase_subtitle[index].innerHTML = info["subtitle"];
+        showcase_link[index].href = info["url"];
+    });
+});
