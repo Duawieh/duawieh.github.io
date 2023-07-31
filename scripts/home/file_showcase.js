468b87ff70658992912710c7187d1a8d9614e6c4
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-07-31 11:10:54 +0800

    showcase json

--- scripts/home/file_showcase.js
+++ scripts/home/file_showcase.js
@@ -1,11 +1,3 @@
-// $("#homepageCenterShowcaseTitle_Title").load("./contents/homepageShowcase/showcase1/title.txt");
-// $("#homepageLeftShowcaseTitle_Title").load("./contents/homepageShowcase/showcase2/title.txt");
-// $("#homepageRightShowcaseTitle_Title").load("./contents/homepageShowcase/showcase3/title.txt");
-
-// $("#homepageCenterShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase1/subtitle.txt");
-// $("#homepageLeftShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase2/subtitle.txt");
-// $("#homepageRightShowcaseTitle_Subtitle").load("./contents/homepageShowcase/showcase3/subtitle.txt");
-
 $.getJSON("./contents/homepageShowcase/config.json", function(data) {
     let showcase_title = new Array();
     let showcase_subtitle = new Array();
