d9be1f367d5ed5f1da99f1bff583332b8c87e05f
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-08-01 14:11:02 +0800

    aboutpage json

--- scripts/home/file_aboutpage.js
+++ scripts/home/file_aboutpage.js
@@ -2,6 +2,7 @@
 var athr = document.getElementById("aboutpageAuthor");
 var prof = document.getElementById("aboutpageProfile");
 var mail = document.getElementById("aboutpageEmail");
+var link = document.getElementById("aboutpageLink");
 
 $.getJSON("./config/aboutME.json", function(data) {
     $.each(data, function(index, info) {
@@ -18,5 +19,16 @@ $.getJSON("./config/aboutME.json", function(data) {
         
         // 获取和覆写个人简介
         prof.innerHTML = info["Profile"];
+
+        // 获取和覆写链接列表
+        let list_link = "";
+        $.each(info["link"], function(jndex, jnfo) {
+            list_link += "<a href=\"";
+            list_link += jnfo["url"];
+            list_link += "\"><img class=\"AboutpageLinkIcons\" src=\"";
+            list_link += jnfo["Icon"];
+            list_link += "\"/></a>";
+        });
+        link.innerHTML = list_link;
     });
 });
