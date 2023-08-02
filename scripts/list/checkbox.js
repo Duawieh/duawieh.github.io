d1cce7cb6153d8a314ddd57ed44be9b8ab1e5eea
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-08-02 16:51:02 +0800

    listpage design

--- scripts/list/checkbox.js
+++ scripts/list/checkbox.js
@@ -1,10 +1,5 @@
 var CHECKBOX = document.getElementById("listpageCheckbox");
 
-// 用户选择的标签集合
-var Set_checked_tag = new Set();
-// 用户输入的字符串
-var String_checked_name = "";
-
 function CTRL_tagCheck(btn) {
     if (getComputedStyle(btn).borderStyle == "solid") {
         btn.style.borderStyle = "none";
@@ -18,13 +13,13 @@ function CTRL_tagCheck(btn) {
 }
 
 function CTRL_search(_input) {
-    String_checked_name = _input;
-    refresh(Array.from(Set_checked_tag, String_checked_name));
+    String_checked_name = _input.value;
+    refresh(Array.from(Set_checked_tag), String_checked_name);
     return false;
 }
 
 function GENERATOR_tagcase(dataTag) {
-    let rst = "<button class=\"ListpageCheckbox\" onclick=\"CTRL_tagCheck(this)\" id=\"tag_";
+    let rst = "<button class=\"ListpageCheckboxButton\" onclick=\"CTRL_tagCheck(this)\" id=\"tag_";
     rst += dataTag + "\" style=\"border-style: none;\">";
     rst += dataTag + "</button>";
     return rst;
