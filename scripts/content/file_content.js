8361873ee229d3aa41838dd6ee143a29d361de84
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-08-09 13:03:09 +0800

    contentpage file

--- scripts/content/file_content.js
+++ scripts/content/file_content.js
@@ -1,6 +1,15 @@
 var content_path = window.location.search;
+
+if (content_path == "") {
+    alert("您访问的 content 不存在！\nThe requested content is not found!");
+    window.location.href = "https://duawieh.github.io";
+}
+
 content_path = 'https://duawieh.github.io/' + content_path.substring(1, content_path.length);
 
+alert(content_path);
+
 $.get(content_path, function(md) {
+    alert(md);
     document.getElementById("contentpageMainText").innerHTML = marked.parse(md);
 });
