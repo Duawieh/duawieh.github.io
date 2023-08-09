6362ddfce5a1c1a69a0e094ae480feb461bced42
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-08-09 11:56:49 +0800

    contentpage file

--- scripts/content/file_content.js
+++ scripts/content/file_content.js
@@ -1,8 +1,6 @@
 var content_path = window.location.search;
-console.log(content_path);
 content_path = './' + content_path.substring(1, content_path.length);
-console.log(content_path);
 
 $.get(content_path, function(md) {
-    $("#contentpageMain").html(marked(md));
+    document.getElementById("#contentpageMain").innerHTML = marked.parse(md);
 });
