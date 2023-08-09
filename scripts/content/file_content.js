5a3d7e7d8b5b691eb9a7eed859f33e61a6e0be0b
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-08-09 12:43:33 +0800

    contentpage file

--- scripts/content/file_content.js
+++ scripts/content/file_content.js
@@ -1,7 +1,6 @@
 var content_path = window.location.search;
-content_path = './' + content_path.substring(1, content_path.length);
+content_path = 'https://duawieh.github.io/' + content_path.substring(1, content_path.length);
 
 $.get(content_path, function(md) {
-    // document.getElementById("#contentpageMain").innerHTML = marked.parse(md);
-    document.getElementById("#contentpageMain").innerHTML = md;
+    document.getElementById("contentpageMainText").innerHTML = marked.parse(md);
 });
