4b8b932c126d6eb00bffb8d358ae364365b9c330
Duawieh Chaser <DuawiehPublic@outlook.com>
2023-08-10 17:17:00 +0800

    url debug

--- scripts/file_url.js
+++ scripts/file_url.js
@@ -4,5 +4,9 @@ var elments_a = document.getElementsByClassName("LinkFromRoot");
 
 // 将读取到的主机和端口地址添加到相对路径 url 前
 $.each(elments_a, function(i, info) {
-    info.href = URL_ROOT + info.href;
+    console.log(i);
+    console.log(info.href);
+    console.log(URL_ROOT);
+    console.log(window.location.hostname);
+    // info.href = URL_ROOT + info.href;
 });
